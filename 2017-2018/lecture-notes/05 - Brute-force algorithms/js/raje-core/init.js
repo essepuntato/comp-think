8272dd511132ec269c9ff2db190af72d3bd84f6c
Silvio Peroni <essepuntato@gmail.com>
2017-11-22 13:00:43 +0100

    Lecture notes of today's lecture

---
+++ 2017-2018/lecture-notes/05 - Brute-force algorithms/js/raje-core/init.js
@@ -0,0 +1,495 @@
+/**
+ * 
+ * Initilize TinyMCE editor with all required options
+ */
+
+// Invisible space constants
+const ZERO_SPACE = '&#8203;'
+const RAJE_SELECTOR = 'body#tinymce'
+
+// Selector constants (to move inside a new const file)
+const HEADER_SELECTOR = 'header.page-header.container.cgen'
+const FIRST_HEADING = `${RAJE_SELECTOR}>section:first>h1:first`
+
+const TINYMCE_TOOLBAR_HEIGTH = 76
+
+let ipcRenderer, webFrame
+
+if (hasBackend) {
+
+  ipcRenderer = require('electron').ipcRenderer
+  webFrame = require('electron').webFrame
+
+  /**
+   * Initilise TinyMCE 
+   */
+  $(document).ready(function () {
+
+    // Override the margin botton given by RASH for the footer
+    $('body').css({
+      'margin-bottom': 0
+    })
+
+    //hide footer
+    $('footer.footer').remove()
+
+    //attach whole body inside a placeholder div
+    $('body').html(`<div id="raje_root">${$('body').html()}</div>`)
+
+    // 
+    setNonEditableHeader()
+
+    tinymce.init({
+
+      // Select the element to wrap
+      selector: '#raje_root',
+
+      // Set window size
+      height: window.innerHeight - TINYMCE_TOOLBAR_HEIGTH,
+
+      // Set the styles of the content wrapped inside the element
+      content_css: ['css/bootstrap.min.css', 'css/rash.css', 'css/raje-core.css'],
+
+      // Set plugins
+      plugins: "raje_inlineFigure fullscreen link codesample raje_externalLink raje_inlineCode raje_inlineQuote raje_section table image noneditable raje_image raje_quoteblock raje_codeblock raje_table raje_listing raje_inline_formula raje_formula raje_crossref raje_footnotes raje_metadata raje_lists raje_save",
+
+      // Remove menubar
+      menubar: false,
+
+      // Custom toolbar
+      toolbar: 'undo redo bold italic link superscript subscript raje_inlineCode raje_inlineQuote raje_inline_formula raje_crossref raje_footnotes | raje_ol raje_ul raje_codeblock raje_quoteblock raje_table raje_image raje_listing raje_formula | raje_section raje_metadata raje_save',
+
+      // Setup full screen on init
+      setup: function (editor) {
+
+        let pasteBookmark
+
+        // Set fullscreen 
+        editor.on('init', function (e) {
+
+          editor.execCommand('mceFullScreen')
+
+          // Move caret at the first h1 element of main section
+          // Or right after heading
+          tinymce.activeEditor.selection.setCursorLocation(tinymce.activeEditor.dom.select(FIRST_HEADING)[0], 0)
+        })
+
+        editor.on('keyDown', function (e) {
+
+          // Prevent shift+enter
+          if (e.keyCode == 13 && e.shiftKey)
+            e.preventDefault()
+
+          if (e.keyCode == 86 && e.metaKey) {
+
+            if ($(tinymce.activeEditor.selection.getNode()).is('pre')) {
+
+              e.preventDefault()
+              e.stopImmediatePropagation()
+
+              pasteBookmark = tinymce.activeEditor.selection.getBookmark()
+            }
+          }
+        })
+
+        /**
+         * 
+         */
+        editor.on('click', function (e) {
+
+          // Capture the triple click event
+          if (e.detail == 3) {
+
+            e.preventDefault()
+            e.stopImmediatePropagation()
+
+            let wrapper = $(tinymce.activeEditor.selection.getRng().startContainer).parents('p,figcaption,:header').first()
+            let startContainer = wrapper[0]
+            let endContainer = wrapper[0]
+            let range = document.createRange()
+
+            // Check if the wrapper has more text node inside
+            if (wrapper.contents().length > 1) {
+
+              // If the first text node is a not editable strong, the selection must start with the second element
+              if (wrapper.contents().first().is('strong[contenteditable=false]'))
+                startContainer = wrapper.contents()[1]
+
+              // In this case the endContainer will be the last text node
+              endContainer = wrapper.contents().last()[0]
+            }
+
+            range.setStart(startContainer, 0)
+
+            if (wrapper.is('figcaption'))
+              range.setEnd(endContainer, endContainer.length)
+
+            else
+              range.setEnd(endContainer, 1)
+
+            tinymce.activeEditor.selection.setRng(range)
+          }
+
+        })
+
+        // Prevent span 
+        editor.on('nodeChange', function (e) {
+
+          let selectedElement = $(tinymce.activeEditor.selection.getNode())
+
+          // Move caret to first heading if is after or before not editable header
+          if (selectedElement.is('p') && (selectedElement.next().is(HEADER_SELECTOR) || (selectedElement.prev().is(HEADER_SELECTOR) && tinymce.activeEditor.dom.select(FIRST_HEADING).length)))
+            tinymce.activeEditor.selection.setCursorLocation(tinymce.activeEditor.dom.select(FIRST_HEADING)[0], 0)
+
+          // If the current element isn't inside header, only in section this is permitted
+          if (selectedElement.parents('section').length) {
+
+            if (selectedElement.is('span#_mce_caret[data-mce-bogus]') || selectedElement.parent().is('span#_mce_caret[data-mce-bogus]')) {
+
+              // Remove span normally created with bold
+              if (selectedElement.parent().is('span#_mce_caret[data-mce-bogus]'))
+                selectedElement = selectedElement.parent()
+
+              let bm = tinymce.activeEditor.selection.getBookmark()
+              selectedElement.replaceWith(selectedElement.html())
+              tinymce.activeEditor.selection.moveToBookmark(bm)
+            }
+          }
+
+          updateDocumentState()
+        })
+
+        // Update saved content on undo and redo events
+        editor.on('Undo', function (e) {
+          tinymce.triggerSave()
+        })
+
+        editor.on('Redo', function (e) {
+          tinymce.triggerSave()
+        })
+
+        editor.on('Paste', function (e) {
+
+          let target = $(e.target)
+
+          // If the paste event is called inside a listing
+          if (pasteBookmark && target.parents('figure:has(pre:has(code))').length) {
+
+            let data = e.clipboardData.getData('Text')
+
+            // Restore the selection saved on cmd+v
+            tinymce.activeEditor.selection.moveToBookmark(pasteBookmark)
+
+            // Update the content
+            tinymce.activeEditor.selection.setContent(e.clipboardData.getData('Text'))
+
+            pasteBookmark = null
+          }
+        })
+      },
+
+      // Set default target
+      default_link_target: "_blank",
+
+      // Prepend protocol if the link starts with www
+      link_assume_external_targets: true,
+
+      // Hide target list
+      target_list: false,
+
+      // Hide title
+      link_title: false,
+
+      // Set formats
+      formats: {
+        underline: {}
+      },
+
+      // Remove "powered by tinymce"
+      branding: false,
+
+      // Prevent auto br on element insert
+      apply_source_formatting: false,
+
+      // Prevent non editable object resize
+      object_resizing: false,
+
+      // Update the table popover layout
+      table_toolbar: "tableinsertrowbefore tableinsertrowafter tabledeleterow | tableinsertcolbefore tableinsertcolafter tabledeletecol",
+
+      image_advtab: true,
+
+      paste_block_drop: true,
+
+      extended_valid_elements: "svg[*],defs[*],pattern[*],desc[*],metadata[*],g[*],mask[*],path[*],line[*],marker[*],rect[*],circle[*],ellipse[*],polygon[*],polyline[*],linearGradient[*],radialGradient[*],stop[*],image[*],view[*],text[*],textPath[*],title[*],tspan[*],glyph[*],symbol[*],switch[*],use[*]",
+
+      formula: {
+        path: 'node_modules/tinymce-formula/'
+      },
+
+      cleanup_on_startup: false,
+      trim_span_elements: false,
+      verify_html: false,
+      cleanup: false,
+      convert_urls: false
+    })
+  })
+
+  /**
+   * Open and close the headings dropdown
+   */
+  $(window).load(function () {
+
+    // Open and close menu headings Näive way
+    $(`div[aria-label='heading']`).find('button').trigger('click')
+    $(`div[aria-label='heading']`).find('button').trigger('click')
+  })
+
+
+  /**
+   * Update content in the iframe, with the one stored by tinymce
+   * And save/restore the selection
+   */
+  function updateIframeFromSavedContent() {
+
+    // Save the bookmark 
+    let bookmark = tinymce.activeEditor.selection.getBookmark(2, true)
+
+    // Update iframe content
+    tinymce.activeEditor.setContent($('#raje_root').html())
+
+    // Restore the bookmark 
+    tinymce.activeEditor.selection.moveToBookmark(bookmark)
+  }
+
+  /**
+   * Accept a js object that exists in frame
+   * @param {*} element 
+   */
+  function moveCaret(element, toStart) {
+    tinymce.activeEditor.selection.select(element, true)
+    tinymce.activeEditor.selection.collapse(toStart)
+
+    tinymce.activeEditor.focus()
+  }
+
+  /**
+   * 
+   */
+  function selectRange(startContainer, startOffset, endContainer, endOffset) {
+
+    let range = document.createRange()
+    range.setStart(startContainer, startOffset)
+
+    // If these properties are not in the signature use the start
+    if (!endContainer && !endOffset) {
+      endContainer = startContainer
+      endOffset = startOffset
+    }
+
+    range.setEnd(endContainer, endOffset)
+    tinymce.activeEditor.selection.setRng(range)
+  }
+
+  /**
+   * 
+   * @param {*} element 
+   */
+  function moveCursorToEnd(element) {
+
+    let heading = element
+    let offset = 0
+
+    if (heading.contents().length) {
+
+      heading = heading.contents().last()
+
+      // If the last node is a strong,em,q etc. we have to take its text 
+      if (heading[0].nodeType != 3)
+        heading = heading.contents().last()
+
+      offset = heading[0].wholeText.length
+    }
+
+    tinymce.activeEditor.focus()
+    tinymce.activeEditor.selection.setCursorLocation(heading[0], offset)
+  }
+
+  /**
+   * 
+   * @param {*} element 
+   */
+  function moveCursorToStart(element) {
+
+    let heading = element
+    let offset = 0
+
+    tinymce.activeEditor.focus()
+    tinymce.activeEditor.selection.setCursorLocation(heading[0], offset)
+  }
+
+
+  /**
+   * Create custom into notification
+   * @param {*} text 
+   * @param {*} timeout 
+   */
+  function notify(text, type, timeout) {
+
+    // Display only one notification, blocking all others
+    if (tinymce.activeEditor.notificationManager.getNotifications().length == 0) {
+
+      let notify = {
+        text: text,
+        type: type ? type : 'info',
+        timeout: timeout ? timeout : 1000
+      }
+
+      tinymce.activeEditor.notificationManager.open(notify)
+    }
+  }
+
+  /**
+   * 
+   * @param {*} elementSelector 
+   */
+  function scrollTo(elementSelector) {
+    $(tinymce.activeEditor.getBody()).find(elementSelector).get(0).scrollIntoView();
+  }
+
+  /**
+   * 
+   */
+  function getSuccessiveElementId(elementSelector, SUFFIX) {
+
+    let lastId = 0
+
+    $(elementSelector).each(function () {
+      let currentId = parseInt($(this).attr('id').replace(SUFFIX, ''))
+      lastId = currentId > lastId ? currentId : lastId
+    })
+
+    return `${SUFFIX}${lastId+1}`
+  }
+
+  /**
+   * 
+   */
+  function headingDimension() {
+    $('h1,h2,h3,h4,h5,h6').each(function () {
+
+      if (!$(this).parents(HEADER_SELECTOR).length) {
+        var counter = 0;
+        $(this).parents("section").each(function () {
+          if ($(this).children("h1,h2,h3,h4,h5,h6").length > 0) {
+            counter++;
+          }
+        });
+        $(this).replaceWith("<h" + counter + " data-rash-original-wrapper=\"h1\" >" + $(this).html() + "</h" + counter + ">")
+      }
+    });
+  }
+
+  /**
+   * 
+   */
+  function checkIfPrintableChar(keycode) {
+
+    return (keycode > 47 && keycode < 58) || // number keys
+      (keycode == 32 || keycode == 13) || // spacebar & return key(s) (if you want to allow carriage returns)
+      (keycode > 64 && keycode < 91) || // letter keys
+      (keycode > 95 && keycode < 112) || // numpad keys
+      (keycode > 185 && keycode < 193) || // ;=,-./` (in order)
+      (keycode > 218 && keycode < 223); // [\]' (in order)
+  }
+
+  /**
+   * 
+   */
+  function markTinyMCE() {
+    $('div[id^=mceu_]').attr('data-rash-original-content', '')
+  }
+
+  /**
+   * 
+   */
+  function setNonEditableHeader() {
+    $(HEADER_SELECTOR).addClass('mceNonEditable')
+  }
+
+  /**
+   * 
+   */
+  function checkIfApp() {
+    return ipcRenderer.sendSync('isAppSync')
+  }
+
+  /**
+   * 
+   */
+  function selectImage() {
+    return ipcRenderer.sendSync('selectImageSync')
+  }
+
+
+
+  /**
+   * Send a message to the backend, notify the structural change
+   * 
+   * If the document is draft state = true
+   * If the document is saved state = false
+   */
+  function updateDocumentState() {
+
+    // Get the Iframe content not in xml 
+    let JqueryIframe = $(`<div>${tinymce.activeEditor.getContent()}</div>`)
+    let JquerySavedContent = $(`#raje_root`)
+
+    // True if they're different, False is they're equal
+    ipcRenderer.send('updateDocumentState', JqueryIframe.html() != JquerySavedContent.html())
+  }
+
+  /**
+   * 
+   */
+  function saveAsArticle(options) {
+    return ipcRenderer.send('saveAsArticle', options)
+  }
+
+  /**
+   * 
+   */
+  function saveArticle(options) {
+    return ipcRenderer.send('saveArticle', options)
+  }
+
+  /**
+   * Start the save as process getting the data and sending it
+   * to the main process
+   */
+  ipcRenderer.on('executeSaveAs', (event, data) => {
+    saveManager.saveAs()
+  })
+
+  /**
+   * Start the save process getting the data and sending it
+   * to the main process
+   */
+  ipcRenderer.on('executeSave', (event, data) => {
+    saveManager.save()
+  })
+
+  /**
+   * 
+   */
+  ipcRenderer.on('notify', (event, data) => {
+    notify(data.text, data.type, data.timeout)
+  })
+
+  /**
+   * 
+   */
+  ipcRenderer.on('updateContent', (event, data) => {
+    tinymce.triggerSave()
+  })
+}
